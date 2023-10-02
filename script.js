03846caccf5bdc421d3c6496e6db149040c2d98d
Varun Shah <104298930+varunlmxd@users.noreply.github.com>
2023-10-03 01:31:09 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,14 +4,6 @@ document.addEventListener('DOMContentLoaded', function() {
   let play = false
   var myAudio = new Audio(chrome.runtime.getURL("bahubali.mp3"));
   function main(){
-    document.getElementById('found').innerHTML = "One"
-      document.getElementById('found').innerHTML = "TWo"
-      
-      document.getElementById('found').innerHTML = "Thre"
-      
-      document.getElementById('found').innerHTML = "4"
-  
-  document.getElementById('found').innerHTML = "five"
   myAudio.play();
   const y = document.getElementById('pop');
   y.addEventListener('click', () => {
@@ -25,7 +17,6 @@ document.addEventListener('DOMContentLoaded', function() {
       y.innerHTML = "Play";
     }
   });
-  document.getElementById('found').innerHTML = "6"
   }
 
   main();
