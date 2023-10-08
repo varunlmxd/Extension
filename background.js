a321b935cc8136cd8781553e1ceec702f50bf4c0
Varun Shah <104298930+varunlmxd@users.noreply.github.com>
2023-10-08 22:32:21 +0530

    final

--- background.js
+++ background.js
@@ -1,5 +1,5 @@
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-  if (message.foundInternship === true) {
+  if (message.foundAccepted === true) {
     // Open the popup
     try{
       chrome.action.openPopup();
@@ -7,9 +7,6 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     catch(err){
       console.log(err)
     }
-    console.log("here")
-   
-    
     setTimeout(() => {
       chrome.scripting.executeScript({
         target: { tabId: sender.tab.id },
@@ -17,20 +14,7 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
       });
       const extensionId = chrome.runtime.id;
      chrome.runtime.sendMessage(extensionId, { action: 'playAudio' });
-    }, 1000);
-    console.log("here1")
-    // Send a message to the popup script using the extension ID
-    
-    // 
-    //   // Send a message to the popup script
-    //   chrome.runtime.sendMessage(extensionId, { action: 'playAudio' });
-
-    //   // Inject the popup script into the current tab
-    //   chrome.scripting.executeScript({
-    //     target: { tabId: sender.tab.id, allFrames: true },
-    //     
-    //   });
-    // 
+    }, 1000); 
   }
   
 });
--- content.js
+++ content.js
@@ -1,44 +1,37 @@
 'use strict';
 
-let hasConditionBeenMet = false;
-function checkForInternship() {
-  
-  if (hasConditionBeenMet) {
-    return;
-  }
-  const elements = document.querySelectorAll('span[data-e2e-locator="submission-result"]');
-  let foundInternship = false;
-  for (const element of elements) {
-    if (element.textContent.includes("Accepted")) {
-      foundInternship = true;
-      break;
-    }
-  }
-  if (foundInternship && chrome.runtime && chrome.runtime.sendMessage && !hasConditionBeenMet) {
-    hasConditionBeenMet = true;
-    const extensionId = chrome.runtime.id;
+let hasSongBeenPlayed = false;
+function checkForAccepted() {
 
-  // Send a message to the popup script to play the audio tag
-  chrome.runtime.sendMessage(extensionId, {
-    type: "play-audio"
-  });
-    chrome.runtime.sendMessage({ foundInternship: true });
-  }
+	if (hasSongBeenPlayed) {
+		return;
+	}
+	const elements = document.querySelectorAll('span[data-e2e-locator="submission-result"]');
+	let foundAccepted = false;
+	for (const element of elements) {
+		if (element.textContent.includes("Accepted")) {   foundAccepted = true;   
+			break;  
+		} 
+	} 
+	if (foundAccepted && chrome.runtime && chrome.runtime.sendMessage && !hasSongBeenPlayed) {  hasSongBeenPlayed = true;
+		const extensionId = chrome.runtime.id;
+
+		// Send a message to the popup script to play the audio tag
+		chrome.runtime.sendMessage(extensionId, {
+			type: "play-audio"
+		});  chrome.runtime.sendMessage({
+			foundAccepted: true
+		}); 
+	}
 }
-
 
 // You can call this function when you want to play the audio
 
-
-checkForInternship();
-const observer = new MutationObserver(() => {
-  checkForInternship();
+checkForAccepted();
+const observer = new MutationObserver(() =>{
+  checkForAccepted();
 });
 
-observer.observe(document.body, {
-  subtree: true,
-  childList: true
+observer.observe(document.body, { subtree: true,
+	 childList: true
 });
-
-
-
